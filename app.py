0c674b7dffec9bfbf0380b621604772f76fc6d80
avedilin <superflur@gmail.com>
2021-04-07 16:39:26 +0300

    final version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 import lightgbm as lgb
+import numpy as np
 
 app = Flask(__name__)
 
@@ -8,8 +9,10 @@ bst = lgb.Booster(model_file='final_lgbm_model.txt')
 
 @app.route('/getPredictions', methods=['POST'])
 def get_predictions():
+    np.set_printoptions(suppress=True)
+
     body = request.get_json()
 
     ypred = bst.predict(body.get('data'), num_iteration=bst.best_iteration)
 
-    return {"data":ypred.tolist()}
+    return {"prediction": str(ypred)}
